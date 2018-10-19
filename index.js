c4b7adf1d876196cd0858dd70b23c926af3ccfda
TangXiaoji <txiaozhe@gmail.com>
2018-10-19 21:39:10 +0800

    not finished!

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Creator: Tang Xiaoji
+ * Time: 2017-12-22
+ */
+
+module.exports = require('./lib/eip');
---
+++ test/test.js
@@ -0,0 +1,9 @@
+/**
+ * Creator: Tang Xiaoji
+ * Time: 2017-12-22
+ */
+
+const eip = require('../index');
+
+const e = new eip();
+e.getEIPList();
